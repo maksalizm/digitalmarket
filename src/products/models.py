34d38fabecb929657aa9d1ec485eb832bbf03406
pms <pms@wishket.com>
2016-08-22 09:05:42 +0900

    basic migrations

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
+    def __unicode__(self):
+        return self.title
