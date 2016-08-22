4f0746aa282bb1c70df9b920ee519a6de4580d09
pms <pms@wishket.com>
2016-08-22 09:25:04 +0900

    basic django admin

--- src/products/models.py
+++ src/products/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
-    sale_price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99, null=True, blank=True)
+
     def __unicode__(self):
         return self.title
