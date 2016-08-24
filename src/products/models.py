03707840e5460a562ee44279b7d0f9a9f3888293
maksalizm <maksalizm@gmail.com>
2016-08-25 02:13:12 +0900

    base 작업 중

--- src/products/models.py
+++ src/products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save
 
 # Create your models here.
 
 class Product(models.Model):
     title = models.CharField(max_length=30)
+    slug = models.SlugField(blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
     sale_price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99, null=True, blank=True)
--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,10 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Product
+
+# def detail_view(request, object_id=None):
+# 	product = get_object_or_404(Product, id=object_id)
+# 	template = "detail_view.html"
