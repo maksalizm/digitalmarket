dc3c449b756c9f01524d390d66ebddf2a9ba97c2
pms <pms@wishket.com>
2016-09-20 09:56:07 +0900

    class base detail view

--- src/products/models.py
+++ src/products/models.py
@@ -5,10 +5,11 @@ from django.db.models.signals import pre_save, post_save
 
 class Product(models.Model):
     title = models.CharField(max_length=30)
-    slug = models.SlugField(blank=True)
+    slug = models.SlugField(default='slug-field')  # unique=True
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
     sale_price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99, null=True, blank=True)
 
     def __unicode__(self):
         return self.title
+
--- src/products/views.py
+++ src/products/views.py
@@ -1,11 +1,27 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 
 from .forms import ProductAddForm, ProductModelForm
 from .models import Product
 
+
+class ProductDetailView(DetailView):
+    model = Product
+
+    def get_object(self, *args, **kwargs):
+        slug = self.kwargs.get("slug")
+        ModelClass = self.model
+        if slug is not None:
+            try:
+                obj = get_object_or_404(ModelClass, slug=slug)
+            except ModelClass.MultipleObjectsReturned:
+                obj = ModelClass.objects.filter(slug=slug).order_by("-title").first()
+        else:
+            obj = super(ProductDetailView, self).get_object(*args, **kwargs)
+        return obj
 
 class ProductListView(ListView):
     model = Product
