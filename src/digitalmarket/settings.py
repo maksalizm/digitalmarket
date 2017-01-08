5a949d8bc1cc520cba7ed789ef7b012992c69ba3
maksalizm <maksalizm@gmail.com>
2017-01-08 17:59:44 +0900

    setting

--- src/digitalmarket/settings.py
+++ src/digitalmarket/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'digitalmarket.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'digitalmarket',
-        'USER': 'dmadmin',
-        'PASSWORD': 'dmadmin123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'digitalmarket',
+#         'USER': 'dmadmin',
+#         'PASSWORD': 'dmadmin123',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Internationalization
